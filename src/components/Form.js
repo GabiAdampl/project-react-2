8c02a9a40f89815e98e800d6927ba291eb034a3e
Gabi Adamczak <gabiadamczak@gmail.com>
2023-07-26 21:48:04 +0200

    projekt2

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,25 +2,47 @@ import React, { useState } from 'react';
 import Result from './Result';
 
 const Form = () => {
-  const [amount, setAmount] = useState('');
-  const [currency, setCurrency] = useState('');
+	const [amount, setAmount] = useState('');
+	const [currency, setCurrency] = useState('');
 
-  const handleAmountChange = (event) => {
-    setAmount(event.target.value);
-  };
+	const handleAmountChange = event => {
+		setAmount(event.target.value);
+	};
 
-  const handleCurrencyChange = (event) => {
-    setCurrency(event.target.value);
-  };
+	const handleCurrencyChange = event => {
+		setCurrency(event.target.value);
+	};
 
-  return (
-    <main className="form-container d-flex justify-content-center gap-2">
-      {/* ... */}
-      {/* Reszta kodu z Form.js */}
-      {/* ... */}
-      <Result amount={amount} currency={currency} />
-    </main>
-  );
+	return (
+		<main className='form-container d-flex justify-content-center gap-2'>
+			<label className='d-flex align-items-center gap-2 font-weight-bold' htmlFor='amountInput'>
+				Kwota:
+				<input
+					type='number'
+					id='amountInput'
+					value={amount}
+					onChange={handleAmountChange}
+					required
+					min='0.01'
+					step='0.01'
+				/>
+			</label>
+
+			<label className='d-flex align-items-center gap-2 font-weight-bold' htmlFor='currencySelect'>
+				Waluta:
+				<select id='currencySelect' value={currency} onChange={handleCurrencyChange} required>
+					<option value='' disabled selected>
+						Wybierz walutę
+					</option>
+					<option value='EUR'>EUR</option>
+					<option value='USD'>USD</option>
+					<option value='CHF'>CHF</option>
+				</select>
+			</label>
+
+			<Result amount={amount} currency={currency} />
+		</main>
+	);
 };
 
 export default Form;
--- src/components/Result.js
+++ src/components/Result.js
@@ -1,27 +1,39 @@
 import React, { useState } from 'react';
 
 const Result = ({ amount, currency }) => {
-  const [result, setResult] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+	const [result, setResult] = useState('');
+	const [errorMessage, setErrorMessage] = useState('');
 
-  const getExchangeRate = async (currency) => {
-    // ...
-  };
+	const getExchangeRate = async currency => {
+		// ...
+	};
 
-  const convertToPLN = async () => {
-    // ...
-  };
+	const convertToPLN = async () => {
+		try {
+			const exchangeRate = await getExchangeRate(currency);
+			const resultValue = amount * exchangeRate;
+			setResult(resultValue.toFixed(2));
+			setErrorMessage(''); // Jeśli konwersja przebiegła pomyślnie, usuń komunikat o błędzie
+		} catch (error) {
+			console.error(error);
+			setErrorMessage('Nie udało się przeliczyć. Spróbuj ponownie.'); // Ustaw komunikat o błędzie w przypadku niepowodzenia konwersji
+		}
+	};
 
-  return (
-    <>
-      <button id="convertButton" onClick={convertToPLN}>Przelicz</button>
-      <p className="d-flex justify-content-center align-items-center">TO</p>
-      <p id="resultOutput">{result ? `${result} PLN` : ''}</p>
+	return (
+		<>
+			<button id='convertButton' onClick={convertToPLN}>
+				Przelicz
+			</button>
+			<p className='d-flex justify-content-center align-items-center'>TO</p>
+			<p id='resultOutput'>{result ? `${result} PLN` : ''}</p>
 
-      {/* Komunikat o błędzie */}
-      <p id="error-message" className="error-message">{errorMessage}</p>
-    </>
-  );
+			{/* Komunikat o błędzie */}
+			<p id='error-message' className='error-message'>
+				{errorMessage}
+			</p>
+		</>
+	);
 };
 
 export default Result;
